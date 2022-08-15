fe358d697044afba4382d4af894e9944e5c162df
viniciusgit2 <4xv6patelfin@gmail.com>
2022-08-15 11:44:46 -0300

    tudo certo

---
+++ pages/componentes/porta.jsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/porta.module.css"
+export default function porta (){
+    return(
+  
+  
+<div className={styles.porta}>porta
+   <div className={styles.chao}>neve</div>
+</div>
+    )
+}
+
---
+++ pages/componentes/presente.jsx
@@ -0,0 +1,13 @@
+import styles from "../../styles/presente.module.css"
+export default function presente (){
+    return(
+   <div className={styles.presente}>que barbada
+    <div className={styles.topo}>menos</div>
+ <div className={styles.corpo}>as dimensoes </div>
+ <div className={styles.laco1}>já é</div>
+ <div className={styles.laco2}>a 120 tentativa</div>
+
+ </div>
+    )
+    
+}
